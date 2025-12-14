696c1b1a58e532d5261e2da66bc026376ca37ff1
Anderson Lins <75218924+demoraes1@users.noreply.github.com>
2025-12-14 18:09:27 -0300

    v1.2.1 mensagens da api traduzidas

--- src/handlers/validation.py
+++ src/handlers/validation.py
@@ -16,14 +16,14 @@ def validate(requestData):
     # STEP 1 :: Validate the existence of an API Key
     product = DBAPI.getProductThroughAPI(requestData.get('apiKey'))
     if(product is None or product == []):
-        return responseMessage(401, 'ERR_API_KEY', 'ERROR :: The API Key you have entered is invalid. The validation request did not go through.')
+        return responseMessage(401, 'ERR_API_KEY', 'ERRO :: A chave de API informada é inválida. A requisição de validação não foi processada.')
     # ##############################################################################
 
     # STEP 2 :: Extract the descrypted data (fail if it is invalid)
     try:
         decryptedData = decrypt_data(requestData.get('payload'), product)
     except Exception:
-        return responseMessage(401, 'ERR_PUB_PRIV_KEY', 'ERROR :: Decription has failed. Your key may be invalid.')
+        return responseMessage(401, 'ERR_PUB_PRIV_KEY', 'ERRO :: A descriptografia falhou. Sua chave pode ser inválida.')
 
     # The data in the decryptedData section is organized as:
     # decryptedData[0] - Serial Key
@@ -33,7 +33,7 @@ def validate(requestData):
     # STEP 3 :: Validate the Serial Key by matching it to an existing License object
     keyObject = DBAPI.getKeysBySerialKey(decryptedData[0], product.id)
     if(keyObject is None or keyObject == []):
-        return responseMessage(401, 'ERR_SERIAL_KEY', 'ERROR :: The Serial Key you have entered is invalid. The validation request went through but was rejected.', decryptedData)
+        return responseMessage(401, 'ERR_SERIAL_KEY', 'ERRO :: A chave serial informada é inválida. A requisição de validação foi processada mas foi rejeitada.', decryptedData)
     # ##############################################################################
 
     if(DBAPI.getRegistration(keyObject.id, decryptedData[1]) is None):
@@ -52,10 +52,10 @@ def handleExistingState(keyObject, decryptedData):
     activationDate = getattr(keyObject, 'activationdate', None)
     
     if(validateExpirationDate(keyObject.expirydate, expiryType, expiryDays, activationDate)):
-        return responseMessage(200, 'OKAY', 'OKAY STATUS :: This device is still registered and everything is okay.', decryptedData, keyObject.expirydate)
+        return responseMessage(200, 'OKAY', 'SUCESSO :: Este dispositivo ainda está registrado e tudo está funcionando corretamente.', decryptedData, keyObject.expirydate)
     else:
         DBAPI.applyExpirationState(keyObject.id)
-        return responseMessage(400, 'ERR_KEY_EXPIRED', 'ERROR :: This license is no longer valid.', decryptedData, keyObject.expirydate)
+        return responseMessage(400, 'ERR_KEY_EXPIRED', 'ERRO :: Esta licença não é mais válida.', decryptedData, keyObject.expirydate)
 
 
 def handleNonExistingState(keyObject, decryptedData):
@@ -66,7 +66,7 @@ def handleNonExistingState(keyObject, decryptedData):
 
     # STEP 1 :: Validate the status of the License (if it's revoked/disabled, then interrupt the validation with an error)
     if(keyObject.status == 2):
-        return responseMessage(403, 'ERR_KEY_REVOKED', 'ERROR :: The key was revoked. Your request was valid but the license is disabled until further notice.', decryptedData, keyObject.expirydate)
+        return responseMessage(403, 'ERR_KEY_REVOKED', 'ERRO :: A chave foi revogada. Sua requisição foi válida mas a licença está desativada até segunda ordem.', decryptedData, keyObject.expirydate)
 
     # STEP 2 :: Check if the License has expired. If that's the case, then the validation should be interrupted with an error.
     expiryType = getattr(keyObject, 'expirytype', 0)
@@ -75,19 +75,19 @@ def handleNonExistingState(keyObject, decryptedData):
     
     if(not validateExpirationDate(keyObject.expirydate, expiryType, expiryDays, activationDate)):
         DBAPI.applyExpirationState(keyObject.id)
-        return responseMessage(400, 'ERR_KEY_EXPIRED', 'ERROR :: This license is no longer valid and will not admit any new devices.', decryptedData, keyObject.expirydate)
+        return responseMessage(400, 'ERR_KEY_EXPIRED', 'ERRO :: Esta licença não é mais válida e não aceitará novos dispositivos.', decryptedData, keyObject.expirydate)
 
     # STEP 3 :: Check if the License's device list can hold more devices.
     if(keyObject.devices == keyObject.maxdevices):
-        return responseMessage(400, 'ERR_KEY_DEVICES_FULL', 'ERROR :: The maximum number of devices for this license key has been reached.', decryptedData, keyObject.expirydate)
+        return responseMessage(400, 'ERR_KEY_DEVICES_FULL', 'ERRO :: O número máximo de dispositivos para esta chave de licença foi atingido.', decryptedData, keyObject.expirydate)
 
     # If all steps above go through, then we accept the validation
     DBAPI.addRegistration(keyObject.id, decryptedData[1], keyObject)
-    return responseMessage(201, 'SUCCESS', 'SUCCESS :: Your registration was successful!', decryptedData, keyObject.expirydate)
+    return responseMessage(201, 'SUCCESS', 'SUCESSO :: Seu registro foi realizado com sucesso!', decryptedData, keyObject.expirydate)
 
 
 # Utility Functions
-def responseMessage(HTTPCode=200, ResponseCode='OKAY', Message='Everything is okay (DEFAULT RESPONSE)', decryptedData=None, expirationDate=None):
+def responseMessage(HTTPCode=200, ResponseCode='OKAY', Message='Tudo está funcionando corretamente (RESPOSTA PADRÃO)', decryptedData=None, expirationDate=None):
     """
         Creates a JSON string that contains all the individual components of a standard response.
     """
